7808662b61ea5bdcb33028a5a1ce870c5c224d3a
Fernando Severino <fernandosevjipa@gmail.com>
2022-04-14 10:32:26 -0300

    fix import of functions.ts

--- src/controllers/StudentsController.ts
+++ src/controllers/StudentsController.ts
@@ -1,5 +1,5 @@
 import express from "express";
-const functions = require("../functions.ts");
+const functions = require("../functions");
 const Students = require("./../models/Students");
 
 module.exports = {
